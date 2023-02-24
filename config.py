2f1c280a931e07329fb05115c0943ace93b733d9
Recordu <117724279+Recordu@users.noreply.github.com>
2023-02-24 13:27:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", "3898418"))
-API_HASH = os.getenv("API_HASH", "5a82313211da04d63297bd4de436228c")
-SESSION = os.getenv("SESSION", "")
-OWNER = os.getenv("OWNER", "")
+API_ID = int(os.getenv("API_ID", "27449839"))
+API_HASH = os.getenv("API_HASH", "8c8ea28d8a537279c444dc338adb0952")
+SESSION = os.getenv("SESSION", "BQAss6ZTo2Ft4HLk-he9yhcHiC61bPJ-eimT5BF5rCkNrTkoCsHm2oJ-cOQT07FStJE_6nlmvnqHLyYnnxSY1-_nccKmHJJfnv8P5exrHfbikkTJHI2iPR5pJdlFkJyK3WDB2U-zaXhTRAosknfWH6OppkvMbVFzwss8w3YjK4vuiBB4JRCxaQNY_sQgL0K1_rhI9prkytXhtc3T8rx5TWzTRkM8d9_PbCXiPFeQDjvHtdjV919D95ShXn4gbnYHAQp158H5ECDEyB5q1SSQqcWGDOvlwdjlzWsDfGkg7Daw83-tXaSZp84WTX9takT0LNlBQrHYnd0zV2QyxiZUC9IoAAAAAVjGw0oA")
+OWNER = os.getenv("OWNER", "5784388426")
 SUPPORT = os.getenv("SUPPORT", "TheDeadlyBots")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, os.getenv("5784388426").split()))
